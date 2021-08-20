3532123cfde9d1be664f675dddc22940df854e02
minseong-dev <aa971020@gmail.com>
2021-08-20 16:40:08 +0900

    이전 내용 삭제

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,22 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+const boardRoute = require('./boardRoute')
+
 router.get('/', function(req, res, next) {
-
-  const user = {
-    name : "sshworld",
-    weight : 100
-  }
-
-  res.render('index', { title: 'Express', user : user });
-
-});
-
-router.get('/login', function(req, res) {
-  res.send(`<script type="text/javascript">alert("로그인 되었습니다.");
-  location.href="/";</script>`)
-
-})
+    res.render('index', { title: 'Express' });
+  });
 
 module.exports = router;
