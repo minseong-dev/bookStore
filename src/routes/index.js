be7c106478e422504ba49e7ba389696443ac5c48
minseong-dev <aa971020@gmail.com>
2021-08-24 08:39:25 +0900

    도서목록 추가

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,9 +3,10 @@ var router = express.Router();
 
 const mainRoute = require('./mainRoute')
 const userRoute = require('./userRoute')
-
+const bookRoute = require('./bookRoute')
 
 router.use('/', mainRoute)
 router.use('/user', userRoute)
+router.use('/book', bookRoute)
 
 module.exports = router;
