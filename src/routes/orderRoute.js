c329f15c2d71c4bc3c09e505ede7383e0a23f1ac
minseong-dev <aa971020@gmail.com>
2021-11-08 03:46:56 +0900

    api수정

--- src/routes/orderRoute.js
+++ src/routes/orderRoute.js
@@ -4,12 +4,15 @@ var router = express.Router();
 const orderController = require('../controllers/orderController')
 
 /* 주문 */
-router.post('/order/:order_uid', orderController.order);
+router.post('/order', orderController.order);
 
 /* 주문 페이지 */
-router.get('/orderPage/:user_uid', orderController.orderPage);
+router.get('/orderPage/:book_uid', orderController.orderPage);
 
 /* 주문목록 페이지 */
-router.get('/orderListPage/:user_uid', orderController.orderListPage);
+router.get('/orderListPage', orderController.orderListPage);
+
+/* 주문목록 삭제 */
+router.delete('/deleteOrder/:order_uid', myPageController.deleteDest);
 
 module.exports = router;
